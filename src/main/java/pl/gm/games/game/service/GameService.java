a7fea258e6205fd8746074c37dc9142e81b94be5
Grzesiek1337 <gmiedzian@gmail.com>
2023-04-04 21:30:04 +0200

    refactor

--- src/main/java/pl/gm/games/game/service/GameService.java
+++ src/main/java/pl/gm/games/game/service/GameService.java
@@ -1,6 +1,5 @@
 package pl.gm.games.game.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.gm.games.game.exception.GameNotFoundException;
 import pl.gm.games.game.model.Game;
@@ -18,32 +17,33 @@ public class GameService {
     }
 
     public List<Game> getGames() {
-        return this.gameRepository.findAll();
+        return gameRepository.findAll();
     }
 
     public Game getGameById(Long id) {
-        return (Game)this.gameRepository.findById(id).orElseThrow(() -> {
-            return new GameNotFoundException(id);
-        });
+        return gameRepository
+                .findById(id)
+                .orElseThrow(() -> new GameNotFoundException(id));
     }
 
     public Game createGame(Game game) {
-        return (Game)this.gameRepository.save(game);
+        return gameRepository.save(game);
     }
 
     public Game updateGame(Long id, Game updatedGame) {
-        return (Game)this.gameRepository.findById(id).map((game) -> {
-            game.setName(updatedGame.getName());
-            game.setGenre(updatedGame.getGenre());
-            game.setDescription(updatedGame.getDescription());
-            game.setReleaseDate(updatedGame.getReleaseDate());
-            return (Game)this.gameRepository.save(game);
-        }).orElseThrow(() -> {
-            return new GameNotFoundException(id);
-        });
+        return gameRepository
+                .findById(id)
+                .map((game) -> {
+                    game.setName(updatedGame.getName());
+                    game.setGenre(updatedGame.getGenre());
+                    game.setDescription(updatedGame.getDescription());
+                    game.setReleaseDate(updatedGame.getReleaseDate());
+                    return gameRepository.save(game);
+                })
+                .orElseThrow(() -> new GameNotFoundException(id));
     }
 
     public void deleteGame(Long id) {
-        this.gameRepository.deleteById(id);
+        gameRepository.deleteById(id);
     }
 }
